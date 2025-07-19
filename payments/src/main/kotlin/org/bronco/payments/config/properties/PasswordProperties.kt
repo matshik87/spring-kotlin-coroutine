aba2835d6e4500475685c5792a23d599a829413d
maciej.parandyk <maciej.parandyk@yahoo.com>
2025-07-19 13:27:29 +0200

    using BCrypt for encoding password

--- payments/src/main/kotlin/org/bronco/payments/config/properties/PasswordProperties.kt
+++ payments/src/main/kotlin/org/bronco/payments/config/properties/PasswordProperties.kt
@@ -3,13 +3,11 @@ package org.bronco.payments.config.properties
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.stereotype.Component
-import java.nio.charset.Charset
 
 @Component
 @EnableConfigurationProperties(PasswordProperties::class)
 @ConfigurationProperties(prefix = "payments.password")
 class PasswordProperties {
     var default: String = "test-password"
-    var algorithm: String = "invalid-algorithm"
-    var charset: Charset = Charsets.UTF_8
+    var cost: Int = 12
 }
--- payments/src/main/kotlin/org/bronco/payments/services/password/PaymentPasswordService.kt
+++ payments/src/main/kotlin/org/bronco/payments/services/password/PaymentPasswordService.kt
@@ -1,18 +1,16 @@
 package org.bronco.payments.services.password
 
+import at.favre.lib.crypto.bcrypt.BCrypt
 import org.bronco.payments.config.properties.PasswordProperties
 import org.springframework.stereotype.Service
-import java.security.MessageDigest
 
 @Service
 class PaymentPasswordService(
     private val passwordProperties: PasswordProperties
 ) : PasswordService {
-    private val messageDigest: MessageDigest = MessageDigest.getInstance(passwordProperties.algorithm)
 
     override fun encode(password: String?): String {
         val passwordToEncode = password ?: passwordProperties.default
-        val encoded = messageDigest.digest(passwordToEncode.toByteArray(passwordProperties.charset))
-        return String(encoded)
+        return BCrypt.withDefaults().hashToString(passwordProperties.cost, passwordToEncode.toCharArray())
     }
 }
